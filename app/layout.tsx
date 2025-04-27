40ece80fc211062c09c7ebd53c3e106ea9822c26
sidmn <sidmantri3@gmail.com>
2025-04-28 01:35:54 +0530

    initital commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Zen_Kaku_Gothic_New } from "next/font/google";
 
-const Zen = Zen_Kaku_Gothic_New({weight:['300','400','500','700']})
+const Zen = Zen_Kaku_Gothic_New({weight:['300','400','500','700'], subsets: ['latin']})
 
 export const metadata: Metadata = {
   title: "Sid's device",
